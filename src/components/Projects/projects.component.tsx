9b466b61e5f107a7e9e1da07c53f4d642eb0aa76
Imcasero <diegocaserosmr@gmail.com>
2023-11-05 13:47:01 +0100

    fix: refactor projects

--- src/components/Projects/projects.component.tsx
+++ src/components/Projects/projects.component.tsx
@@ -1,19 +1,22 @@
 import { ReactNode } from "react";
 import css from "./projects.module.css";
 import { Technologies } from "../model";
-
-const technologiesArray: string[] = Object.values(Technologies);
 export default function Projects({
   children,
 }: {
   children: ReactNode;
 }): ReactNode {
+  const technologiesArray: string[] = Object.values(Technologies);
   return (
     <section id="projects" className={css.container}>
       <h3>Projects</h3>
       <div className={css.technologies}>
         {technologiesArray.map((tech, index) => (
-          <button key={index} className={css[tech.toLowerCase()]}>
+          <button
+            key={index}
+            className={css[tech.toLowerCase()]}
+            id={tech.toLowerCase()}
+          >
             {tech}
           </button>
         ))}
