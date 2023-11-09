a474289cb851fd28cfbb4156d6702574a2621ee0
Imcasero <diegocaserosmr@gmail.com>
2023-11-09 20:18:25 +0100

    init: initial state for new design

--- src/components/model/interfaces.ts
+++ src/components/model/interfaces.ts
@@ -1,17 +1,54 @@
 export interface IProject {
   name: string;
   description: string;
-  image: string; // URL de la imagen del proyecto
-  deploy?: string; // Enlace de implementación (opcional)
-  repository: string; // Enlace al repositorio del proyecto
-  technologies: Technologies[]; // Array de tecnologías utilizadas en el proyecto
+  year: number;
+  image: string;
+  deploy?: string;
+  repository: string;
+  technologies: Technologies[];
+}
+export enum Month {
+  January = 0,
+  February = 1,
+  March = 2,
+  April = 3,
+  May = 4,
+  June = 5,
+  July = 6,
+  August = 7,
+  September = 8,
+  October = 9,
+  November = 10,
+  December = 11,
+}
+
+export interface IExperience {
+  bussines: string;
+  from: { month: Month; year: Date };
+  to: { month: Month; year: Date } | "Current";
+  description: string;
+  technologies: Technologies[];
 }
 
 export enum Technologies {
   REACT = "React",
+  ANGULAR = "Angular",
   JAVASCRIPT = "JavaScript",
-  POKEAPI = "PokeAPI",
   TYPESCRIPT = "TypeScript",
+  GITHUB = "Github",
+  SASS = "SASS",
   CSS = "CSS",
+  API = "API",
   ANIMATIONS = "Animations",
+  GIT = "Git",
+  VSCODE = "Vscode",
+  SCRUM = "Scrum",
+  AGILE = "Agile",
+  TDD = "TDD",
+  FIGMA = "Figma",
+  BOOTSTRAP = "Bootstrap",
+  DOTNET = ".NET",
+  CSHARP = "CSharp",
+  VB = "VB.NET",
+  SQL = "SQL",
 }
