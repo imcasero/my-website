61dee8595be14c9397ab56f71b192c7e70c8e02b
Imcasero <diegocaserosmr@gmail.com>
2023-11-12 15:37:29 +0100

    build(deps): func react router add

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './input.css'
+import { BrowserRouter } from 'react-router-dom'
+import AppRouter from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      // Agrega alias aquí si es necesario
+    },
+  },
+  esbuild: {
+    jsxFactory: 'jsx',
+    jsxFragment: 'React.Fragment',
+  },
+});
