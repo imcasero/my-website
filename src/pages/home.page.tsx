17895225e924b39503d70abdac19dc273e80533d
Imcasero <diegocaserosmr@gmail.com>
2023-12-06 20:38:48 +0100

    feat: model for home with responsive design

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -7,8 +7,8 @@ import twitter from '../assets/twitter.svg';
 
 export function Home(): ReactElement {
     return (
-        <section className="max-w-screen-lg flex mt-24 justify-center items-center">
-            <div className="w-1/3">
+        <section className="max-w-screen-lg flex lg:flex-row flex-col  mt-24 justify-center items-center">
+            <div className="lg:w-1/3 w-auto">
                 <h1 className="text-title text-3xl font-bold">DIEGO CASERO</h1>
                 <h2 className="text-subtitle text-base font-medium">I write code and love it</h2>
                 <img className="mt-6" src={profile} alt="profile" />
@@ -24,7 +24,7 @@ export function Home(): ReactElement {
                     </a>
                 </div>
             </div>
-            <article className="flex-col items-center w-2/3 ">
+            <article className="flex-col items-center w-2/3 mt-10 lg:mt-0">
                 <p className="text-base font-normal">Hi, I'm Diego Casero, a frontend developer who loves building cool things with code. Started coding with Arduino at 15, it sparked my love for tech. I enjoy an organized setup and building computers.</p>
                 <p className="text-base font-normal mt-4">I take pleasure in executing tasks with a focus on achieving the highest quality possible and consistently tackling challenges. Apart from coding, I have an interest in sports and traveling.</p>
                 <div className="flex mt-8 gap-6 ">
