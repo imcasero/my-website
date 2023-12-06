dd192ae1244e0678b53081eb097e242e97bfd642
Imcasero <diegocaserosmr@gmail.com>
2023-12-06 18:40:51 +0100

    feat: basic template update icon

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
 import Icon from '/myIcon.svg';
-import moon from '../../assets/moon.svg'
 import { useNavigate } from "react-router-dom";
 export default function HeaderComponent(): ReactElement {
     const navigate = useNavigate();
@@ -18,7 +17,7 @@ export default function HeaderComponent(): ReactElement {
         <header className="w-full flex items-center justify-center text-text">
             <nav className="w-3/4 flex items-center justify-around">
                 <button onClick={handleHome}>
-                    <img src={Icon} alt="icono" />
+                    <img src={Icon} alt="icono" className="w-1/4" />
                 </button>
                 <ul className="w-1/2 flex justify-around ">
                     <li onClick={handleProjects} className="cursor-pointer ">
@@ -28,7 +27,6 @@ export default function HeaderComponent(): ReactElement {
                         Carrer
                     </li>
                 </ul>
-                <img src={moon} alt="moon" />
             </nav>
         </header>
     )
