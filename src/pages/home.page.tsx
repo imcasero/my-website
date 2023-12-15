7ea804adec2177b4c529d87d540c8f91d9151080
Imcasero <diegocaserosmr@gmail.com>
2023-12-15 18:30:16 +0100

    hotfix: fix path for CV in home page

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -28,7 +28,7 @@ export function Home(): ReactElement {
                 <p className="text-base font-normal">Hi, I'm Diego Casero, a frontend developer who loves building cool things with code. Started coding with Arduino at 15, it sparked my love for tech. I enjoy an organized setup and building computers.</p>
                 <p className="text-base font-normal mt-4">I take pleasure in executing tasks with a focus on achieving the highest quality possible and consistently tackling challenges. Apart from coding, I have an interest in sports and traveling.</p>
                 <div className="flex mt-8 gap-6 ">
-                    <a className="hover:text-title text-link flex gap-2" href="/public/diego_casero_en.pdf" target="_blank">
+                    <a className="hover:text-title text-link flex gap-2" href="/diego_casero_en.pdf" target="_blank">
                         See mi CV
                         <img src={arrow} alt="arrow" />
                     </a>
