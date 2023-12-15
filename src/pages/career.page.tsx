6be25d249a91a8bfae98319da8ac227471f05e9e
Imcasero <diegocaserosmr@gmail.com>
2023-12-15 18:08:58 +0100

    feat: add career model

--- src/pages/career.page.tsx
+++ src/pages/career.page.tsx
@@ -1,7 +1,16 @@
 import { ReactElement } from "react";
+import { IExperience, experiences } from "../model";
+import CareersComponent from "../components/careers/careers.component";
+import CareerComponent from "../components/career/career.component";
 
 export function Career(): ReactElement {
     return (
-        <div>Hola desde Carrer</div>
+        <>
+            <CareersComponent>
+                {experiences.map((dataCareer: IExperience) => {
+                    return <CareerComponent key={dataCareer.id} data={dataCareer} />;
+                })}
+            </CareersComponent>
+        </>
     )
 }
