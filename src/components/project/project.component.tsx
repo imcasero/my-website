2cf3f80a90debceacfa755a51b1b67d06e307cea
Imcasero <diegocaserosmr@gmail.com>
2023-12-16 13:50:54 +0100

    feat: new projects template

--- src/components/project/project.component.tsx
+++ src/components/project/project.component.tsx
@@ -1,27 +1,28 @@
 import { ReactElement } from "react";
 import { IProject } from "../../model";
-import github from '../../assets/github.svg';
-import deploy from '../../assets/deploy.svg';
+import arrow from '../../assets/arrow.svg';
 import TechComponent from "../tech/tech.component";
 
 export default function ProjectComponent({ data }: { data: IProject }): ReactElement {
     return (
-        <li className="flex flex-col my-4 gap-8 w-3/4">
-            <img className="min-w-1/3" src={data.image} alt="project image" />
-            <div className="w-2/3 flex flex-col">
+        <li className="flex flex-col my-4 gap-8 lg:w-3/4 w-full">
+            <img className="w-auto" src={data.image} alt="project image" />
+            <div className="w-full flex flex-col">
                 <h3 className="text-2xl mb-2 font-semibold text-subtitle gap-2">{data.name} <span className="text-lg text-link font-light">{data.year}</span></h3>
                 <p className="mb-2 font-medium">{data.description}</p>
-                <div className="flex gap-2 mb-4">
-                    <a href={data.repository} target="_balnk" className="transition-transform duration-300 transform-gpu hover:scale-105">
-                        <img src={github} alt="github" />
+                <div className="flex mt-2 gap-6">
+                    <a className="hover:text-title text-link flex gap-2" href={data.repository} target="_blank">
+                        Repository
+                        <img src={arrow} alt="arrow" />
                     </a>
                     {data.deploy && (
-                        <a href={data.deploy} target="_blank" className="transition-transform duration-300 transform-gpu hover:scale-105">
-                            <img src={deploy} alt="deploy" />
+                        <a className="hover:text-title text-link flex gap-2" href={data.deploy} target="_blank">
+                            Web
+                            <img src={arrow} alt="arrow" />
                         </a>
                     )}
                 </div>
-                <ul className="flex gap-1">
+                <ul className="flex gap-1 mx-0 mt-6">
                     {data.technologies.map((tech: string) => {
                         return <TechComponent tech={tech} key={tech} />
                     })}
--- src/components/projects/projects.component.tsx
+++ src/components/projects/projects.component.tsx
@@ -8,7 +8,7 @@ export default function ProjectsComponent({
         <section className="flex flex-col items-start w-3/4 mt-10 mb-10">
             <h2 className="text-title text-3xl font-bold mb-2 ">Projects</h2>
             <h3 className="text-subtitle text-base font-medium mb-4" >Here are some of the projects I've worked on.</h3>
-            <ul className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">{children}</ul>
+            <ul className="grid  grid-cols-1 lg:grid-cols-2 mt-2 gap-8 w-full">{children}</ul>
         </section >
     );
 }
