48afebed374933fb958482f2e15b3a2494550d37
Imcasero <diegocaserosmr@gmail.com>
2023-12-16 12:17:53 +0100

    fix: fix title for projects

--- src/components/project/project.component.tsx
+++ src/components/project/project.component.tsx
@@ -9,7 +9,7 @@ export default function ProjectComponent({ data }: { data: IProject }): ReactEle
         <li className="flex flex-col lg:flex-row my-4 gap-8 w-3/4">
             <img className="min-w-1/3" src={data.image} alt="project image" />
             <div className="w-2/3 flex flex-col">
-                <h3 className="text-2xl text-title font-semibold mb-2 gap-2">{data.name} <span className="text-lg text-link font-light">{data.year}</span></h3>
+                <h3 className="text-2xl mb-2 font-semibold text-subtitle mb-2 gap-2">{data.name} <span className="text-lg text-link font-light">{data.year}</span></h3>
                 <p className="mb-2 font-medium">{data.description}</p>
                 <div className="flex gap-2 mb-4">
                     <a href={data.repository} target="_balnk" className="transition-transform duration-300 transform-gpu hover:scale-105">
