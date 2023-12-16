698fc9af6d481f1e24c974450ab0c56afb976ead
Imcasero <diegocaserosmr@gmail.com>
2023-12-16 12:51:18 +0100

    feat: style update for home

--- src/components/homeDescription/homeDescription.component.tsx
+++ src/components/homeDescription/homeDescription.component.tsx
@@ -1,7 +1,7 @@
 import arrow from '../../assets/arrow.svg';
 export default function HomeDescriptionComponent() {
     return (
-        <article className="flex-col items-center w-2/3 mt-10 lg:mt-0">
+        <article className="flex-col items-center w-1/3 mt-10 lg:mt-0">
             <p className="text-base font-normal">Hi, I'm Diego Casero, a frontend developer who loves building cool things with code. Started coding with Arduino at 15, it sparked my love for tech. I enjoy an organized setup and building computers.</p>
             <p className="text-base font-normal mt-4">I take pleasure in executing tasks with a focus on achieving the highest quality possible and consistently tackling challenges. Apart from coding, I have an interest in sports and traveling.</p>
             <div className="flex mt-8 gap-6 ">
