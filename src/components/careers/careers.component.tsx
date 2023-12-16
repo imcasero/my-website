237ea78d023b1791a3a45d28196193b6aeaa7b6d
Imcasero <diegocaserosmr@gmail.com>
2023-12-16 11:09:38 +0100

    feat: better styles and update literel

--- src/components/careers/careers.component.tsx
+++ src/components/careers/careers.component.tsx
@@ -6,8 +6,8 @@ export default function CareersComponent({
 }): ReactNode {
     return (
         <section className="flex flex-col items-start w-3/4 mt-10 mb-10">
-            <h2 className="text-title text-3xl font-bold mb-2 ">Carrer</h2>
-            <h3 className="text-subtitle text-base font-medium mb-4" >Here are some about my carrer.</h3>
+            <h2 className="text-title text-3xl font-bold mb-2 ">Career</h2>
+            <h3 className="text-subtitle text-base font-medium mb-8" >Here are some about my career.</h3>
             <ul className="flex flex-col justify-center w-full" >{children}</ul>
         </section >
     );
