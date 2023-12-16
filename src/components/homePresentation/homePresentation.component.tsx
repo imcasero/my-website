255591a08d5947c23305073ff3e1837328776f38
Imcasero <diegocaserosmr@gmail.com>
2023-12-16 19:19:53 +0100

    feat: add hover for presentation links

--- src/components/homePresentation/homePresentation.component.tsx
+++ src/components/homePresentation/homePresentation.component.tsx
@@ -14,13 +14,13 @@ export default function HomePresentationComponent() {
             <h2 className="text-subtitle text-base font-medium">I write code and love it</h2>
             <img className="mt-6" src={profile} alt="profile" />
             <div className="flex items-center justify-between w-48 mt-6">
-                <a href="https://github.com/imcasero" target="_blank">
+                <a href="https://github.com/imcasero" target="_blank" className="transform transition-transform hover:scale-110">
                     <img src={github} alt="github" />
                 </a>
-                <a href="https://www.linkedin.com/in/imcasero/" target="_blank">
+                <a href="https://www.linkedin.com/in/imcasero/" target="_blank" className="transform transition-transform hover:scale-110">
                     <img src={linkedin} alt="linkedin" />
                 </a>
-                <a href="https://twitter.com/imcasero_dev" target="_blank">
+                <a href="https://twitter.com/imcasero_dev" target="_blank" className="transform transition-transform hover:scale-110">
                     <img src={twitter} alt="twitter" />
                 </a>
             </div>
