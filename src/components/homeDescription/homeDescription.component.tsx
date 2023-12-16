08bc852764958e1409eb279c0bc37f02e1987089
Imcasero <diegocaserosmr@gmail.com>
2023-12-16 19:34:15 +0100

    fix: fix responsive design

--- src/components/homeDescription/homeDescription.component.tsx
+++ src/components/homeDescription/homeDescription.component.tsx
@@ -6,7 +6,7 @@ export default function HomeDescriptionComponent() {
     return (
         <motion.article initial="hidden"
             animate="visible"
-            variants={animationVariants} className="flex-col items-center md:w-1/3 w-full mt-10 lg:mt-0 md:px-0 px-2">
+            variants={animationVariants} className="flex-col items-center md:w-1/3 w-full mt-10 lg:mt-0 md:px-0 px-4">
             <p className="text-base font-normal">Hi, I'm Diego Casero, a frontend developer who loves building cool things with code. Started coding with Arduino at 15, it sparked my love for tech. I enjoy an organized setup and building computers.</p>
             <p className="text-base font-normal mt-4">I take pleasure in executing tasks with a focus on achieving the highest quality possible and consistently tackling challenges. Apart from coding, I have an interest in sports and traveling.</p>
             <div className="flex mt-8 gap-6 ">
