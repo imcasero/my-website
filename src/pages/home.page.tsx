cd21c74bf3f96caab7457749c9f8528009562a18
Imcasero <diegocaserosmr@gmail.com>
2023-12-16 11:13:47 +0100

    fix: responsive design for home

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -7,8 +7,7 @@ import twitter from '../assets/twitter.svg';
 
 export function Home(): ReactElement {
     return (
-        //TODO make more component for this pages, and make better repsonsive
-        <section className="max-w-screen-lg flex lg:flex-row flex-col  mt-48 justify-center items-center">
+        <section className="max-w-screen-lg flex lg:flex-row flex-col  lg:mt-48 my-16 justify-center items-center">
             <div className="lg:w-1/3 w-auto">
                 <h1 className="text-title text-3xl font-bold">DIEGO CASERO</h1>
                 <h2 className="text-subtitle text-base font-medium">I write code and love it</h2>
