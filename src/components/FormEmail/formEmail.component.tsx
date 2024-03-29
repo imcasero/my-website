4715fa6fde56e2b1ffc8ac0d591e8a4259005bbd
Imcasero <diegocaserosmr@gmail.com>
2024-03-29 13:25:34 +0100

    feat: add better design for email

--- src/components/FormEmail/formEmail.component.tsx
+++ src/components/FormEmail/formEmail.component.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import { motion } from 'framer-motion';
+import { variants } from '../../utils/effect.motion';
 
 interface FormEmailProps {
   onClose: () => void;
@@ -7,13 +9,14 @@ interface FormEmailProps {
 
 const FormEmailComponent: React.FC<FormEmailProps> = ({ onClose }) => {
   const [state, handleSubmit] = useForm("mdoqwzgy");
+  const animationVariants = variants(0);
 
   if (state.succeeded) {
     return (
       <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 backdrop-blur-md">
         <div className="bg-white p-6 rounded-lg">
-          <p>Thanks for joining!</p>
-          <button onClick={onClose} className="mt-4 px-4 py-2 bg-gray-800 text-white rounded-md">
+          <p>Thank you for contacting me, I will reply as soon as possible! ;)</p>
+          <button onClick={onClose} className="mt-4 px-4 py-2 bg-gray-800 text-white rounded-md hover:text-title">
             Close
           </button>
         </div>
@@ -22,17 +25,19 @@ const FormEmailComponent: React.FC<FormEmailProps> = ({ onClose }) => {
   }
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 backdrop-blur-md">
-      <div className="bg-white p-6 rounded-lg">
+    <motion.div initial="hidden"
+      animate="visible"
+      variants={animationVariants} className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 backdrop-blur-md">
+      <div className="bg-white p-6 rounded-lg w-1/2">
         <form onSubmit={handleSubmit} className="space-y-4">
-          <label htmlFor="email" className="block">
+          <label htmlFor="email" className="block text-title">
             Email Address
           </label>
           <input
             id="email"
             type="email"
             name="email"
-            className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+            className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-opacity-50 px-4 py-2 text-background"
           />
           <ValidationError
             prefix="Email"
@@ -40,13 +45,13 @@ const FormEmailComponent: React.FC<FormEmailProps> = ({ onClose }) => {
             errors={state.errors}
             className="text-red-600"
           />
-          <label htmlFor="message" className="block">
+          <label htmlFor="message" className="block text-title">
             Message
           </label>
           <textarea
             id="message"
             name="message"
-            className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+            className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-opacity-50 px-2 py-1 text-background h-36"
           />
           <ValidationError
             prefix="Message"
@@ -55,16 +60,16 @@ const FormEmailComponent: React.FC<FormEmailProps> = ({ onClose }) => {
             className="text-red-600"
           />
           <div className="flex justify-between">
-            <button type="submit" disabled={state.submitting} className="px-4 py-2 bg-blue-500 text-white rounded-md disabled:bg-gray-400">
+            <button type="submit" disabled={state.submitting} className="px-4 py-2 bg-blue-500 text-white rounded-md disabled:bg-gray-400 hover:text-title">
               Submit
             </button>
-            <button onClick={onClose} className="px-4 py-2 bg-gray-800 text-white rounded-md">
+            <button onClick={onClose} className="px-4 py-2 bg-gray-800 text-white rounded-md hover:text-title">
               Close
             </button>
           </div>
         </form>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
