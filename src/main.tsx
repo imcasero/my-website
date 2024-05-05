dd907f596c5804ef585f28036f3016165d65d887
Imcasero <diegocaserosmr@gmail.com>
2024-05-05 15:59:48 +0200

    fix: add speed insights for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { Layout } from './pages/layout.page';
 import { Career, Home, Projects } from './pages';
 import './index.css';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const router = createBrowserRouter([
   {
     path: '/',
