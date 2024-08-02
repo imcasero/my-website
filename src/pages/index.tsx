522c73fc4e5cee1bdf5eab83829e0405aeba1b3e
Diego Casero <diegocaserosmr@gmail.com>
2024-08-02 22:02:44 +0200

    feat: 🚀 new version for 2024 to prod
    * feat: 🚀 new version for 2024 (#24)
    
    * feat: reset tech and new footer component
    
    * feat: update links for footer
    
    * add home and refactor social media component
    
    * refactor: refactor social media component
    
    * feat: add Projects section
    
    * feat: add Career section
    
    * fix: fix styles for layout padding and time line
    
    * fix: fix styles for Footer component
    
    * feat: add new cat logo
    
    * feat: add career Tag component
    
    * feat: add projects and modal for email
    
    * fix: fix rout for logo
    
    * fix: fix rout for logo
    
    * fix: fix favicon and title

--- src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export { Home } from './home.page'
-export { Career } from './career.page'
-export { Projects } from './projects.page'
--- src/utils/effect.motion.ts
+++
@@ -1,6 +0,0 @@
-export const variants = (delay: number) => {
-  return {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { delay, duration: 0.5 } },
-  };
-};
