475ab3b11669d995497481ff0b7d9bb997a4e867
Diego Casero <diegocaserosmr@gmail.com>
2024-08-02 21:38:48 +0200

    feat: 🚀 new version for 2024 (#24)
    * feat: reset tech and new footer component
    
    * feat: update links for footer
    
    * add home and refactor social media component
    
    * refactor: refactor social media component
    
    * feat: add Projects section
    
    * feat: add Career section
    
    * fix: fix styles for layout padding and time line
    
    * fix: fix styles for Footer component
    
    * feat: add new cat logo
    
    * feat: add career Tag component
    
    * feat: add projects and modal for email

--- src/components/tech/tech.component.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function TechComponent({ tech }: { tech: string }) {
-    return (
-        <li className="bg-tag py-1 px-4 rounded-lg">{tech}</li>
-    )
-}
---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          50: "#f0fdf9",
+          100: "#ccfbeb",
+          200: "#99f7dc",
+          300: "#5debc9",
+          400: "#2cd5b1",
+          500: "#14c5a2",
+          600: "#0c957d",
+          700: "#0e7766",
+          800: "#105f52",
+          900: "#134e45",
+          950: "#042f2a",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      width: {
+        main: "48rem",
+      },
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(to right, #14C5A2 0%, #5DEB98 95%)",
+      },
+    },
+  },
+  plugins: [],
+};
