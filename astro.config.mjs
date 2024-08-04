61d08d9545da77715353235dc93275d8c4c2a042
Imcasero <diegocaserosmr@gmail.com>
2024-08-04 18:56:48 +0200

    fix: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
