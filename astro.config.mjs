8b26996e3dc55fd3557b2c69488e82731e595623
Imcasero <diegocaserosmr@gmail.com>
2024-11-15 12:34:05 +0100

    init: reset app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  integrations: [tailwind(), react()],
-});
+// https://astro.build/config
+export default defineConfig({});
