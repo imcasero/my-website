6800821a3c6ceebf2435152bd1b4d2b4ca430d89
Imcasero <diegocaserosmr@gmail.com>
2025-10-07 21:52:20 +0200

    feat: refactor Profile component to use Card wrapper and update Command component to destructure props

---
+++ src/lib/components/shared/Card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<article
+  class="flex flex-col gap-2 border border-border rounded-lg p-6 bg-card w-full"
+>
+  {@render children()}
+</article>
