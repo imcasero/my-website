e450a830f1eee8a7e0d79b52682cd162b8a7e426
Imcasero <diegocaserosmr@gmail.com>
2025-10-07 21:52:20 +0200

    feat: refactor Profile component to use Card wrapper and update Command component to destructure props

--- src/lib/components/StaticView/Profile.svelte
+++ src/lib/components/StaticView/Profile.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
+  import Card from "../shared/Card.svelte";
   import Command from "../shared/Command.svelte";
 </script>
 
-<article
-  class="flex flex-col gap-2 border border-border rounded-lg p-6 bg-card w-full"
->
+<Card>
   <div>
     <Command text="whoami" />
     <h1 class="text-4xl">
@@ -21,4 +20,4 @@
     <Command text="cat ~/role.txt" />
     <p class="text-lg">Software Developer</p>
   </div>
-</article>
+</Card>
--- src/lib/components/shared/Command.svelte
+++ src/lib/components/shared/Command.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  let props = $props();
+  let { text } = $props();
 </script>
 
 <p class="text-muted-foreground">
   <span class="text-primary">$</span>
-  {props.text}
+  {text}
 </p>
